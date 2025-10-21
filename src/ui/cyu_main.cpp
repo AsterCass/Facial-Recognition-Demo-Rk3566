210837b9283374d81a13426a4b371604afb892c9
AsterCasc <astercass@qq.com>
2025-10-21 09:46:55 +0800

    feat: channel

--- src/ui/cyu_main.cpp
+++ src/ui/cyu_main.cpp
@@ -32,48 +32,75 @@ void signal_handler(int sig) {
 }
 
 int init_vi() {
-    VI_DEV_ATTR_S dev_attr;
-    VI_CHN_ATTR_S chn_attr;
+    int ret = 0;
+    int devId = 0;
+    int pipeId = devId;
 
-    memset(&dev_attr, 0, sizeof(VI_DEV_ATTR_S));
-
-    if (RK_MPI_VI_SetDevAttr(VI_DEV_ID, &dev_attr) != RK_SUCCESS) {
-        printf("VI SetDevAttr failed\n");
-        return -1;
+    VI_DEV_ATTR_S stDevAttr;
+    VI_DEV_BIND_PIPE_S stBindPipe;
+    memset(&stDevAttr, 0, sizeof(stDevAttr));
+    memset(&stBindPipe, 0, sizeof(stBindPipe));
+    // 0. get dev config status
+    ret = RK_MPI_VI_GetDevAttr(devId, &stDevAttr);
+    if (ret == RK_ERR_VI_NOT_CONFIG) {
+        // 0-1.config dev
+        ret = RK_MPI_VI_SetDevAttr(devId, &stDevAttr);
+        if (ret != RK_SUCCESS) {
+            printf("RK_MPI_VI_SetDevAttr %x\n", ret);
+            return -1;
+        }
+    } else {
+        printf("RK_MPI_VI_SetDevAttr already\n");
     }
-
-    printf("VI SetDevAttr success\n");
-
-    if (RK_MPI_VI_EnableDev(VI_DEV_ID) != RK_SUCCESS) {
-        printf("VI EnableDev failed\n");
-        return -1;
+    // 1.get dev enable status
+    ret = RK_MPI_VI_GetDevIsEnable(devId);
+    if (ret != RK_SUCCESS) {
+        // 1-2.enable dev
+        ret = RK_MPI_VI_EnableDev(devId);
+        if (ret != RK_SUCCESS) {
+            printf("RK_MPI_VI_EnableDev %x\n", ret);
+            return -1;
+        }
+        // 1-3.bind dev/pipe
+        stBindPipe.u32Num = 1;
+        stBindPipe.PipeId[0] = pipeId;
+        ret = RK_MPI_VI_SetDevBindPipe(devId, &stBindPipe);
+        if (ret != RK_SUCCESS) {
+            printf("RK_MPI_VI_SetDevBindPipe %x\n", ret);
+            return -1;
+        }
+    } else {
+        printf("RK_MPI_VI_EnableDev already\n");
     }
-
-    printf("VI EnableDev success\n");
-
-    memset(&chn_attr, 0, sizeof(VI_CHN_ATTR_S));
-    chn_attr.stSize.u32Width = CAM_WIDTH;
-    chn_attr.stSize.u32Height = CAM_HEIGHT;
-    chn_attr.enPixelFormat = RK_FMT_YUV420SP;
-    chn_attr.u32Depth = 1;
-    chn_attr.enCompressMode = COMPRESS_MODE_NONE;
-
-    if (RK_MPI_VI_SetChnAttr(VI_DEV_ID, VI_CHN_ID, &chn_attr) != RK_SUCCESS) {
-        printf("VI SetChnAttr failed\n");
-        return -1;
-    }
-
-    printf("VI SetChnAttr success\n");
-
-    if (RK_MPI_VI_EnableChn(VI_DEV_ID, VI_CHN_ID) != RK_SUCCESS) {
-        printf("VI EnableChn failed\n");
-        return -1;
-    }
-
-    printf("VI EnableChn success\n");
 
     return 0;
 }
+
+
+int vi_chn_init(int channelId, int width, int height) {
+    int ret;
+    int buf_cnt = 2;
+    // VI init
+    VI_CHN_ATTR_S vi_chn_attr;
+    memset(&vi_chn_attr, 0, sizeof(vi_chn_attr));
+    vi_chn_attr.stIspOpt.u32BufCount = buf_cnt;
+    vi_chn_attr.stIspOpt.enMemoryType = VI_V4L2_MEMORY_TYPE_DMABUF; // VI_V4L2_MEMORY_TYPE_MMAP;
+    vi_chn_attr.stSize.u32Width = width;
+    vi_chn_attr.stSize.u32Height = height;
+    vi_chn_attr.enPixelFormat = RK_FMT_YUV420SP;
+    vi_chn_attr.enCompressMode = COMPRESS_MODE_NONE; // COMPRESS_AFBC_16x16;
+    vi_chn_attr.u32Depth = 2; //0, get fail, 1 - u32BufCount, can get, if bind to other device, must be < u32BufCount
+    ret = RK_MPI_VI_SetChnAttr(0, channelId, &vi_chn_attr);
+    ret |= RK_MPI_VI_EnableChn(0, channelId);
+    if (ret) {
+        printf("ERROR: create VI error! ret=%d\n", ret);
+        return ret;
+    }
+
+    return ret;
+}
+
+
 
 CyuMain::CyuMain(QWidget *parent) : QWidget(parent) {
     setWindowTitle("RK3566 Camera Demo");
@@ -110,6 +137,10 @@ CyuMain::CyuMain(QWidget *parent) : QWidget(parent) {
     }
 
     printf("Init VI success\n");
+
+    vi_chn_init(0, CAM_WIDTH, CAM_HEIGHT);
+
+    printf("Init CHN success\n");
 
     while (!g_exit) {
         printf("Start get frame\n");
